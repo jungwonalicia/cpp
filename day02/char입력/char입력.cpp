065cf59994895216b80592d457b8a3ed5e796876
jungwonalicia <aliciawill@naver.com>
2019-06-16 14:11:55 +0900

    Add files via upload

---
+++ day02/char입력/char입력.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	char ch;
+	//std::cout << "맞으면 o, 틀리면 x를 입력하세요. ";
+	//std::cin >> ch;
+
+	std::string s = "ok";
+
+	ch = s.at(0);
+	if (ch == 'o') {
+		std::cout << "맞습니다.";
+	}
+	else {
+		std::cout << "틀립니다.";
+	}
+}
---
+++ day02/doWhile반복/doWhile반복.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	string str;
+	do {
+		cout << "문자열을 입력: ";
+		//getline(cin, str);
+		cin >> str;
+		cout << "당신의 입력값은 " << str << endl;
+	} while (str != "종료");
+}
